6840abb44b47ccf9354db6a67985eb7ace4bbddc
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-04-19 14:00:11 +0200

    feat: setup handling of GitHub webhook

--- server/handlers.go
+++ server/handlers.go
@@ -1,15 +1,28 @@
 package server
 
 import (
-	"encoding/json"
+	"io"
 	"net/http"
+	"strings"
+
+	"github.com/PiquelOrganization/docs.piquel.fr/utils"
 )
 
 func (s *Server) GithubPushHandler(w http.ResponseWriter, r *http.Request) {
-    decoder := json.NewDecoder(r.Body)
-    var payload any
-    err := decoder.Decode(&payload)
-    if err != nil {
-        panic(err)
-    }
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	header := r.Header.Get("X-Hub-Signature-256")
+	header = strings.Split(header, "=")[1]
+
+	isGithub, err := utils.VerifySignature(string(body), header, s.config.WebhookSecret)
+	if err != nil {
+		panic(err)
+	}
+
+	if isGithub {
+		s.Restart()
+	}
 }
