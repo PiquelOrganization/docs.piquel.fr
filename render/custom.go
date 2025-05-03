943a842bf53e5717dfc64013d9c2b1ec95fe5a7d
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-03 10:33:11 +0200

    fix matching and getting data from input matches

--- render/custom.go
+++ render/custom.go
@@ -12,38 +12,36 @@ func (r *RealRenderer) renderCustom(md []byte, config *RenderConfig) []byte {
 }
 
 func (r *RealRenderer) renderSingleline(md []byte, config *RenderConfig) []byte {
-	match := r.singleline.Find(md)
+	match := r.singleline.FindSubmatch(md)
 	if match == nil {
 		return md
 	}
 
-	submatches := r.singleline.FindSubmatch(match)
-	html := r.renderSingleHTML(submatches[0], submatches[1], config)
+	html := r.renderSingleHTML(match[1], match[2], config)
 
-	md = bytes.Replace(md, match, html, 1)
+	md = bytes.Replace(md, match[0], html, 1)
 	return r.renderSingleline(md, config)
 }
 
 func (r *RealRenderer) renderSingleHTML(tag, param []byte, config *RenderConfig) []byte {
-	log.Printf("tag: %s\nparam: %s\n", tag, param)
+	log.Printf("\ntag: %s\nparam: %s\n", tag, param)
 	return []byte("Jesus Christ")
 }
 
 func (r *RealRenderer) renderMultiline(md []byte, config *RenderConfig) []byte {
-	match := r.multiline.Find(md)
+	match := r.multiline.FindSubmatch(md)
 	if match == nil {
 		return md
 	}
 
-	submatches := r.multiline.FindSubmatch(match)
 	var html []byte
-	if len(submatches) == 2 {
-		html = r.renderMultiHTML(submatches[0], nil, submatches[1], config)
-	} else if len(submatches) == 3 {
-		html = r.renderMultiHTML(submatches[0], submatches[1], submatches[2], config)
+	if len(match) == 3 {
+		html = r.renderMultiHTML(match[1], nil, match[2], config)
+	} else if len(match) == 4 {
+		html = r.renderMultiHTML(match[1], match[2], match[3], config)
 	}
 
-	md = bytes.Replace(md, match, html, 1)
+	md = bytes.Replace(md, match[0], html, 1)
 	return r.renderMultiline(md, config)
 }
 
